18ac6b1b57ceda0083611e09171b613772e736d5
tejas <tejasshah@gmail.com>
2022-06-06 20:00:44 +0530

    init

---
+++ Royal.java
@@ -0,0 +1,112 @@
+//oop 
+//class--property {variable,methods}
+//object --> access property 
+
+//class:-5
+//variable:-2 
+//methods:-2 
+class Royal{
+	
+}
+
+class  Person{
+	public static void main(String args[]){
+		System.out.println("Person");
+	}
+
+}
+class Student {
+	public static void main(String args[]){
+		System.out.println("Student");//PrintStream -> println()
+	}
+}
+//javac Person.java 
+//Person.class 
+//java Person -->class loader --> Person.class --> yes 
+			//Person.main()
+//java Student --> CL --> Student.class --> yes -> main()
+
+
+//1) class ->property  1) variable 2)methods 
+//2) class never use property* 
+//3) object will use property of class 
+//4) if we create static property [ add static as prefix in property ] then
+	//class can access those properties using class name. 
+	//Person.main()
+
+/*
+	console print --> println() 
+				==> belongs to -> PrintStream 
+
+	class System{
+
+		static PrintStream out = XXXX ;//variable 
+	}
+
+
+
+*/ 
+
+
+
+/*
+
+	variable :- data name , user , value 
+
+		value; min; max; 
+		class for if --> keywords X 
+		1) value n times [ non constant -- variable ]  
+		salary 
+		minSalary 
+		taxMinSal 
+
+		2) fix value  [ constant ] 
+		SALARY  
+		
+	class 
+		Student 
+		Person 
+		TempStudent 
+
+	object --> same as variable 
+
+	method --> same as variable () 
+		
+	data type :-
+		1) primitive 
+			byte	1byte 
+			short	2byte
+			char  	2byte 
+			int 	4byte
+			long 	8byte 
+			float 	4byte 
+			double 	8byte 
+			boolean 1bit 
+
+		2) non-primitive 
+			class 
+			object 
+			array 
+			enums 
+			XXXXXX 
+	
+
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
---
+++ Student.java
@@ -0,0 +1,20 @@
+//first letter capital 
+//PersonSalary 
+
+class Student{
+
+
+	public static void main(String args[]){
+	
+	}
+
+}
+
+class DhirajSir{
+	
+	public static void main(String args[]){
+	
+	}
+
+}
+//.java   
