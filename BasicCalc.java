de71f27a385ba6e71d2f2f55740898f7169fea8e
tejas <tejasshah@gmail.com>
2022-06-08 19:56:44 +0530

    scanner

---
+++ BasicCalc.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class BasicCalc {
+
+	public static void main(String[] args) {
+
+		//class --- Scanner - nextInt()  nextFloat() nextDouble()   
+							
+		Scanner scr = new Scanner(System.in);//created -- memory -- no -- null  new 
+		//classname objname = new classname(); 
+
+		int a = 10, b = 20, c;
+		System.out.println("Enter two numbers");
+		a = scr.nextInt();
+		b = scr.nextInt();
+		c = a + b;
+		System.out.println("addition = " + c);
+		// 10 + 20 = 30
+		System.out.println(a + " + " + b + " = " + (a + b));
+	}
+}
+//movie -boll  holl [eng dubb]
+
+//class 1 lack+
+
+
+//java 
+
+//util 
+
+//lang
+//String System 
+
+//io
+//25-30 5-7  
+
+//sql
+//15-17 5-10 
+
+//security 
+//50-60
+
+ 
