1ae009b9330b861efdb83a0534603c4731f99f60
tejas <tejasshah@gmail.com>
2022-07-11 19:48:29 +0530

    abstract 1

---
+++ src/abs/AbstractAgain.java
@@ -0,0 +1,30 @@
+package abs;
+
+public class AbstractAgain {
+
+	public static void main(String[] args) {
+
+	}
+}
+
+abstract class VH {
+	abstract protected void start();
+
+	abstract void stop();
+
+	abstract void avg();
+}
+
+class Car extends VH {
+	void stop() {
+
+	}
+
+	protected void start() {
+
+	}
+
+	protected void avg() {
+
+	}
+}
---
+++ src/abs/AbstractDemo.java
@@ -0,0 +1,63 @@
+package abs;
+
+public class AbstractDemo {
+
+	public static void main(String[] args) {
+		ICICI dhirajSir = new ICICI();
+
+		dhirajSir.wid();
+		dhirajSir.giveInt();
+
+//		PNB nirav = new PNB();
+		LIC lic = new LIC();
+		lic.dep();
+	}
+}
+//abstract keyword -> method,class 
+
+//class - abstract --- abstract + non abstract 
+//class - nonAbstract --> we have only non abstract method 
+//if your class is an abstract class then we cant't create instance/object 
+abstract class RBI {
+	void giveInt() {
+		System.out.println("INTR");
+	}
+
+	// abstract method
+	// declaration
+	abstract void wid();
+
+	abstract void dep();
+}
+
+abstract class PNB extends RBI {
+	void wid() {
+
+	}
+}
+
+class LIC extends PNB {
+
+	void dep() {
+
+	}
+}
+
+class ICICI extends RBI {
+	void wid() {
+	}
+
+	void dep() {
+	}
+}
+
+class Lom extends ICICI {
+}
+
+class SBI extends RBI {
+	protected void wid() {
+	}
+
+	void dep() {
+	}
+}
