f62d59fd7a44be879257856f2c88e8ed6f39dff1
tejas <tejasshah@gmail.com>
2022-08-17 19:45:43 +0530

    casting

---
+++ src/CastingDemo.java
@@ -0,0 +1,21 @@
+
+public class CastingDemo {
+
+	public static void main(String[] args) {
+
+		// primitive casting
+		int a = 5;
+		int b = 2;
+		float c = (float) a / b;// 2.5 -> 2 -> int/int -> float->int
+		// casting --> upcast downcast
+		// downcast -> value loss
+		// implicit cast
+		// explicit cast
+		System.out.println(c);
+
+		// NonPrimitive casting ---> class
+
+	}
+}
+
+
